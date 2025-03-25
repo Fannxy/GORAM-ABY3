be6bc917bfff34a99ff0322b67af8a1524694db7
Fannxy <fxy23@mails.tsinghua.edu.cn>
2025-03-25 12:25:20 +0800

    update

--- GORAM_Eval/end2end_analysis.sh
+++ GORAM_Eval/end2end_analysis.sh
@@ -1,10 +1,68 @@
+# cd ./aby3/;
+
+# # ulimit -v 419430400; python ./GORAM/end2end_analysis.py --providers 2 --threads 2 --preprocess True >> /scratch1/log.txt 2>&1
+
+# (
+#     # set trap to catch the kill signal
+#     trap 'echo "Process killed by signal at $(date)" >> /scratch1/log.txt; exit 1' SIGTERM SIGKILL
+
+#     # set the memory limit to 400GB
+#     ulimit -v 419430400
+#     python ./GORAM/end2end_analysis.py --providers 2 --threads 2 --preprocess True
+# ) >> /scratch1/log.txt 2>&1
+
+# exit_code=$?
+
+# if [ $exit_code -ne 0 ]; then
+#     echo "Memory limit exceeded during the end2end analysis." >> /scratch1/log.txt
+#     exit 1
+# fi
+
+# cd ..;
+
+
 cd ./aby3/;
 
-ulimit -v 419430400; python ./GORAM/end2end_analysis.py --providers 2 --threads 2 --preprocess True >> /scratch1/log.txt 2>&1
+cp ./frontend/main.e2e ./frontend/main.cpp;
+python build.py
 
-if [ $? -eq 137 ]; then
-    echo "Memory limit exceeded during the end2end analysis."
-    exit 1
-fi
+# data prepare
+echo -e "\e[32mTwitter\e[0m"
+N_list=(2 4)
+data_folder="/root/GORAM-ABY3/aby3/aby3-GORAM/data/real_world/"
+data_file="twitter_2dpartition.txt"
+meta_file="twitter_meta.txt"
 
-cd ..;
+for N in ${N_list[@]}; do
+    (
+        echo "N: $N"
+        save_folder="/root/GORAM-ABY3/aby3/aby3-GORAM/data/real_world/twitter_${N}/"
+
+        if [ ! -d $save_folder ]; then
+            echo "Creating folder $save_folder"
+            mkdir -p $save_folder
+        fi
+
+        echo "Data folder: $save_folder"
+
+        ./out/build/linux/frontend/frontend -prepare True -data_folder $data_folder -data_file_path $data_file -meta_file_path $meta_file -save_folder $save_folder -N $N;
+
+        cat ./debug.txt
+        rm ./debug.txt
+    ) &
+done
+# save_folder="/root/GORAM-ABY3/aby3/aby3-GORAM/data/real_world/twitter_${N}/"
+
+# if [ ! -d $save_folder ]; then
+#     echo "Creating folder $save_folder"
+#     mkdir -p $save_folder
+# fi
+
+# echo "Data folder: $save_folder"
+
+# ./out/build/linux/frontend/frontend -prepare True -data_folder $data_folder -data_file_path $data_file -meta_file_path $meta_file -save_folder $save_folder -N $N;
+
+# cat ./debug.txt
+# rm ./debug.txt
+
+# cd ../;
